2201a9a70e70c50c3ee715d688f11de7656bb51f
vivl <semiletoffvladislav@yandex.ru>
2022-09-05 14:32:28 +0300

    Осуществляет полное исследование функции одной переменной. Возвращает: уравнения асимптот; выражения для первой и второй производных; абсциссы и ординаты точек экстремума и точек перегиба.

---
+++ study_func.py
@@ -0,0 +1,59 @@
+from sympy import *
+
+
+def study_function(y, singp=0, asimp=True):
+    if asimp:
+        if singp != 0:
+            for i in range(0, len(singp)):
+                lim = limit(y, x, singp[i])
+                if abs(lim) == oo:
+                    print('')
+        lp = limit(y, x, +oo)
+        lm = limit(y, x, -oo)
+        if (abs(lp) != oo) & (abs(lm) != oo):
+            print('')
+        elif (abs(lp) != oo) & (abs(lm) == oo):
+            print('')
+        elif (abs(lp) == oo) & (abs(lm) != oo):
+            print('')
+        kp = limit(y/x, x, oo, '+')
+        km = limit(y/x, x, oo, '-')
+        if (kp != 0) & (km != 0):
+            if (kp != oo) & (km != oo):
+                b = limit(y - kp * x, x, oo)
+                if b != oo:
+                    print('')
+            elif (kp != oo) & (km == oo):
+                b = limit(y - kp * x, x, oo, '+')
+                if b != oo:
+                    print('')
+        у_ = diff(y, x)
+        print("у': ", y_.simplify())
+        у2_ = diff(y, х, 2)
+        print('у": ', у2_.simplify())
+        уЗ_ = diff(y, х, 3)
+        roots_diff = solve(y_, x)
+        k = len(roots_diff)
+        if k > 0:
+            for i in range(0, k):
+                ri = roots_diff[i]
+                y2_0 = y2_.subs(x, ri)
+                if y2_0 > 0:
+                    print('x = %s - точка минимума, y_min = %s' % (ri, y.subs(x,ri)))
+                elif y2_0 < 0:
+                    print('x = %s - точка максимума, y_max = %s' % (ri, y.subs(x,ri)))
+                else:
+                    у3_0 = y3_.subs(x, ri)
+                    if у3_0 != 0:
+                        print('x = %s - точка перегиба, у(х) = %s' % (ri, y.subs(x, ri)))
+                    else:
+                        print('B критической точке %s требуется дополнительное исследование' % ri)
+        roots_2diff = solve(y2_)
+        k = len(roots_2diff)
+        if k > 0:
+            for i in range(0, k):
+                ri = roots_2diff[i]
+        y3_0 = y3_.subs(x, ri)
+        y_0 = y_.subs(x, ri |;
+        if (y3_0 != 0) & (y_0 != 0):
+            print('x = %s - точка перегиба, y(x) = %s' % (ri, y.subs(x,ri)))
